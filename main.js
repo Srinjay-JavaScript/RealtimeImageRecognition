c0f4c6f77ee5bb21584e4b97289e5103fd6cae9f
Srinjay-JavaScript <71552411+Srinjay-JavaScript@users.noreply.github.com>
2021-03-18 17:29:51 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){    
+}
+function setup(){
+   canvas = createCanvas(325, 275);
+   canvas.center();
+}
+function draw(){  
+}
+function mode(){
+    document.getElementsByTagName("link")[1].setAttribute("href", "theme.css");
+}
+function mode2(){
+    document.getElementsByTagName("link")[1].setAttribute("href", "style.css");
+}
